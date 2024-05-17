e6c151de5fda112a62ae206a7fc61d44ef28f610
revi23s <ravana.mustafi@gmail.com>
2024-05-17 12:15:23 +0400

    new lesson

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt("adinizi daxil edin");
+let surname = prompt("soyadinizi daxil edin");
+let date = prompt("Doguldugunuz ili daxil edin");
+let age = 2024 - date;
+alert(`adiniz ${name}, soyadiniz ${surname}, yasiniz ${age} `);
